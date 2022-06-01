9a0ad56fc1bc2d72f7b45e5177de610eb3aef6f7
Simone Kranawetter <simonekranawetter@gmail.com>
2022-06-01 22:41:57 +0200

    Add bean counting exercise

--- eloquent-js-exercises/Chapter 3/beanCounting.js
+++ eloquent-js-exercises/Chapter 3/beanCounting.js
@@ -1,4 +1,21 @@
-function countBs(n){
-    //Todo Fix me!
+function countBs(str){
+    let count = 0;
+    for (let i = 0 ; i < str.length; i++){
+        if(str.charAt(i)=== "B"){ //I'd like to check for "b" too. But it's not required, so no.
+            count++;
+        }
+    }
+    return (count);
 }
 console.log(countBs("BBC"));
+
+function countChar(str, char){
+    let count = 0;
+    for (let i = 0 ; i < str.length; i++){
+        if(str.charAt(i)=== char){
+            count++;
+        }
+    }
+    return (count);
+}
+console.log(countChar("kakkerlak","k"));
